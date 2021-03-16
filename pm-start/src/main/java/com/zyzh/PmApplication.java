ef5e200c9dc273866c4fe12b389df4c5afbdf3e4
wcy <s2288156@163.com>
2021-03-16 14:02:53 +0800

    web依赖引入

---
+++ pm-start/src/main/java/com/zyzh/PmApplication.java
@@ -0,0 +1,14 @@
+package com.zyzh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wcy
+ */
+@SpringBootApplication
+public class PmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PmApplication.class, args);
+    }
+}
