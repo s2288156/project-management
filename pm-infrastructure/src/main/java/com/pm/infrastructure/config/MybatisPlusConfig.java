8f30d919f74e99d4b46a8a6e32c1e4a4dce3d292
wcy <s2288156@163.com>
2021-03-18 16:44:21 +0800

    [feat]用户注册: 配置mybatis注解扫描路径，以及Jackson序列化枚举值为数据库存储值

--- pm-infrastructure/src/main/java/com/pm/infrastructure/config/MybatisPlusConfig.java
+++ pm-infrastructure/src/main/java/com/pm/infrastructure/config/MybatisPlusConfig.java
@@ -3,7 +3,9 @@ package com.pm.infrastructure.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,5 +20,10 @@ public class MybatisPlusConfig {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return builder -> builder.featuresToEnable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
     }
 }
