9a416a9d8994c353c70f057bff5e45b882906119
wcy <s2288156@163.com>
2021-03-18 13:44:50 +0800

    [feat]用户登录web接口定义

---
+++ pm-controller/src/main/java/com/pm/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.pm.controller;
+
+import com.alibaba.cola.dto.Response;
+import com.pm.application.command.UserLoginCmd;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wcy
+ */
+@RequestMapping("/user")
+@RestController
+public class UserController {
+
+    @PostMapping("/login")
+    public Response login(@RequestBody @Validated UserLoginCmd userLoginCmd) {
+
+        return Response.buildSuccess();
+    }
+}
---
+++ pm-domain/src/main/java/com/zyzh/pm/domain/user/User.java
@@ -0,0 +1,44 @@
+package com.zyzh.pm.domain.user;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author wcy
+ **/
+@Data
+public class User {
+
+    private String id;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
+
+    /**
+     * 登录账户名
+     **/
+    private String username;
+
+    /**
+     * 密码
+     **/
+    private String password;
+
+    /**
+     * 头像
+     **/
+    private String icon;
+
+    /**
+     * 邮箱
+     **/
+    private String email;
+
+    /**
+     * 姓名
+     **/
+    private String name;
+
+}
