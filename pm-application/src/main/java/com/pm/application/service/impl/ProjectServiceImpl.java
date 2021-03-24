4686aa89b6adf6e6b42839f18e935988d7b9eff5
wcy <s2288156@163.com>
2021-03-24 11:08:37 +0800

    [PM-4]项目信息录入: review

--- pm-application/src/main/java/com/pm/application/service/impl/ProjectServiceImpl.java
+++ pm-application/src/main/java/com/pm/application/service/impl/ProjectServiceImpl.java
@@ -1,15 +1,9 @@
 package com.pm.application.service.impl;
 
 import com.alibaba.cola.dto.SingleResponse;
-import com.alibaba.cola.exception.BizException;
 import com.pm.application.command.ProjectAddCmdExe;
-import com.pm.application.consts.ErrorCodeEnum;
-import com.pm.application.convertor.ProjectConvertor;
 import com.pm.application.dto.cmd.ProjectAddCmd;
 import com.pm.application.service.IProjectService;
-import com.pm.infrastructure.dataobject.ProjectDO;
-import com.pm.infrastructure.mapper.ProjectMapper;
-import com.zyzh.pm.domain.gateway.GroupGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
