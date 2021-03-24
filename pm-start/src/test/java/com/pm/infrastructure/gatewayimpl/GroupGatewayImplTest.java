346bf97380724b26fdbf65836a8148b4601959c5
wcy <s2288156@163.com>
2021-03-24 09:52:19 +0800

    [PM-4]项目信息录入: group查询是否存在，测试用例通过

--- pm-start/src/test/java/com/pm/infrastructure/gatewayimpl/GroupGatewayImplTest.java
+++ pm-start/src/test/java/com/pm/infrastructure/gatewayimpl/GroupGatewayImplTest.java
@@ -1,10 +1,12 @@
 package com.pm.infrastructure.gatewayimpl;
 
 import com.pm.NoneWebBaseTest;
+import com.pm.infrastructure.dataobject.GroupDO;
 import com.pm.infrastructure.mapper.GroupMapper;
 import com.zyzh.pm.domain.gateway.GroupGateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,14 +21,17 @@ class GroupGatewayImplTest extends NoneWebBaseTest {
     @Autowired
     private GroupMapper groupMapper;
 
+    @Transactional
     @Test
     void testExistById() {
         boolean none = groupGateway.existById("hhhhh");
         assertFalse(none);
 
-        String id = "111";
+        GroupDO groupDO = new GroupDO();
+        groupDO.setName("agagaga");
+        groupMapper.insert(groupDO);
 
-        boolean yes = groupGateway.existById(id);
+        boolean yes = groupGateway.existById(groupDO.getId());
         assertTrue(yes);
     }
 }
