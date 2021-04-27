07f0a928d66e4880e35805ba65d0314f47c63bde
yangtonglong <16601167534@163.com>
2021-04-27 11:32:39 +0800

    PM-18:模块删除修改CollectionUtils包为springframwork

--- pm-application/src/main/java/com/pm/application/command/ModuleDeleteCmdExe.java
+++ pm-application/src/main/java/com/pm/application/command/ModuleDeleteCmdExe.java
@@ -1,13 +1,13 @@
 package com.pm.application.command;
 
 import com.alibaba.cola.dto.Response;
-import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.pm.application.consts.ErrorCodeEnum;
 import com.pm.application.dto.cmd.ModuleDeleteCmd;
 import com.pm.infrastructure.mapper.DependenceMapper;
 import com.pm.infrastructure.mapper.ModuleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -25,7 +25,7 @@ public class ModuleDeleteCmdExe {
 
     public Response execute(ModuleDeleteCmd moduleDeleteCmd) {
         List<String> projectIdList = dependenceMapper.selectDependenceInfo(moduleDeleteCmd.getId());
-        if (CollectionUtils.isNotEmpty(projectIdList)) {
+        if (!CollectionUtils.isEmpty(projectIdList)) {
             return Response.buildFailure(ErrorCodeEnum.MODULE_DEPENDENCE_ERROR.getErrorCode(), ErrorCodeEnum.MODULE_DEPENDENCE_ERROR.getErrorMsg());
         }
         moduleMapper.deleteById(moduleDeleteCmd.getId());
