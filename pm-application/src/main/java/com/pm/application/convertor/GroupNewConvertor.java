bdfd7ff35d57d799dae64aa1981dd00308a4aad3
wcy <s2288156@163.com>
2021-05-18 14:30:47 +0800

    recode: mapstruct性能测试

--- pm-application/src/main/java/com/pm/application/convertor/GroupNewConvertor.java
+++ pm-application/src/main/java/com/pm/application/convertor/GroupNewConvertor.java
@@ -3,6 +3,7 @@ package com.pm.application.convertor;
 import com.pm.application.dto.cmd.GroupAddCmd;
 import com.pm.infrastructure.dataobject.GroupDO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 /**
@@ -12,5 +13,6 @@ import org.mapstruct.factory.Mappers;
 public interface GroupNewConvertor {
     GroupNewConvertor INSTANCE = Mappers.getMapper(GroupNewConvertor.class);
 
+    @Mapping(source = "name", target = "name")
     GroupDO convert2Do(GroupAddCmd groupAddCmd);
 }
---
+++ pm-start/src/test/java/com/pm/demo/MapstructPerformanceTest.java
@@ -0,0 +1,54 @@
+package com.pm.demo;
+
+import com.pm.application.convertor.GroupNewConvertor;
+import com.pm.application.dto.cmd.GroupAddCmd;
+import com.pm.infrastructure.dataobject.GroupDO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.util.StopWatch;
+
+/**
+ * @author wcy
+ */
+@Slf4j
+public class MapstructPerformanceTest {
+
+
+    @ValueSource(ints = {10000, 100000, 1000000})
+    @ParameterizedTest
+    void testMapstructVSBeanUtils(int loopsNum) {
+        log.warn("遍历次数loopsNum = {}", loopsNum);
+        GroupAddCmd groupAddCmd = new GroupAddCmd();
+        groupAddCmd.setName("hahah");
+
+        StopWatch mapstructWatch = new StopWatch("mapstruct");
+        StopWatch beanUtilsWatch = new StopWatch("beanUtils");
+
+        beanUtilsWatch.start();
+        beanUtilsConvertor(groupAddCmd, loopsNum);
+        beanUtilsWatch.stop();
+
+        log.warn("beanUtils = {}", beanUtilsWatch.getTotalTimeNanos());
+
+        mapstructWatch.start();
+        mapstructConvertor(groupAddCmd, loopsNum);
+        mapstructWatch.stop();
+
+        log.warn("mapstruct = {}", mapstructWatch.getTotalTimeNanos());
+    }
+
+    private void mapstructConvertor(GroupAddCmd groupAddCmd, int loopsNum) {
+        for (int i = 0; i < loopsNum; i++) {
+            GroupDO groupDO = GroupNewConvertor.INSTANCE.convert2Do(groupAddCmd);
+        }
+    }
+    private void beanUtilsConvertor(GroupAddCmd groupAddCmd, int loopsNum) {
+        for (int i = 0; i < loopsNum; i++) {
+            GroupDO groupDO = new GroupDO();
+            BeanUtils.copyProperties(groupAddCmd, groupDO);
+        }
+    }
+}
