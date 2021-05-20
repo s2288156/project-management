277257ed7da47975741217b3f4820f0d60e91e17
wcy <s2288156@163.com>
2021-05-20 10:00:11 +0800

    PM-26:feat:BeanUtils全面替换为mapstruct GroupConvertor.java  LoginUserVO.java

--- pm-application/src/main/java/com/pm/application/convertor/GroupConvertor.java
+++ pm-application/src/main/java/com/pm/application/convertor/GroupConvertor.java
@@ -1,6 +1,7 @@
 package com.pm.application.convertor;
 
 import com.pm.application.dto.cmd.GroupAddCmd;
+import com.pm.application.dto.vo.GroupVO;
 import com.pm.infrastructure.dataobject.GroupDO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -13,4 +14,6 @@ public interface GroupConvertor {
     GroupConvertor INSTANCE = Mappers.getMapper(GroupConvertor.class);
 
     GroupDO convert2Do(GroupAddCmd groupAddCmd);
+
+    GroupVO convertDo2Vo(GroupDO groupDO);
 }
