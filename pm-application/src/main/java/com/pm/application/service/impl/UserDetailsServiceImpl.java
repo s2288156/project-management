cc97de7635a95be6d474891d72f13d3e957f5bea
wcy <s2288156@163.com>
2021-05-25 17:53:30 +0800

    jdk 设置为8

--- pm-application/src/main/java/com/pm/application/service/impl/UserDetailsServiceImpl.java
+++ pm-application/src/main/java/com/pm/application/service/impl/UserDetailsServiceImpl.java
@@ -7,7 +7,6 @@ import com.pm.infrastructure.mapper.UserMapper;
 import com.pm.infrastructure.security.SecurityUser;
 import com.zyzh.exception.BizException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -15,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * @author wcy
@@ -37,7 +35,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<UserDO> userOptional = userMapper.selectForUsername(username);
-        if (userOptional.isEmpty()) {
+        if (!userOptional.isPresent()) {
             throw new BizException(ErrorCodeEnum.USERNAME_NOT_FOUND);
         }
         UserDO userDO = userOptional.get();
