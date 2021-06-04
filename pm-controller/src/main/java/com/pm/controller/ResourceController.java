47ca3ba35418ff05ca4c7dfc2b02ac9a2ceb110b
wcy <s2288156@163.com>
2021-06-04 17:36:38 +0800

    PM-30:feat:resource列表查询 web接口定义

--- pm-controller/src/main/java/com/pm/controller/ResourceController.java
+++ pm-controller/src/main/java/com/pm/controller/ResourceController.java
@@ -1,28 +1,16 @@
 package com.pm.controller;
 
-import com.alibaba.cola.dto.Response;
 import com.alibaba.cola.dto.SingleResponse;
 import com.pm.application.dto.cmd.ResourceAddCmd;
+import com.pm.application.dto.vo.ResourceVO;
 import com.pm.application.service.IResourceService;
-import com.pm.infrastructure.cache.ICacheService;
+import com.pm.infrastructure.entity.PageResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
-import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 /**
  * @author wcy
@@ -38,5 +26,10 @@ public class ResourceController {
     @PostMapping
     public SingleResponse<String> addResource(ResourceAddCmd addCmd) {
         return SingleResponse.of(resourceService.addResource(addCmd));
+    }
+
+    @GetMapping("/list")
+    public PageResponse<ResourceVO> pageResource() {
+        return null;
     }
 }
