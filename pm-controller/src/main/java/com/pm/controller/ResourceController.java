02fe180c38645d28e5ff3293a5e52a95a394e739
wcy <s2288156@163.com>
2021-06-05 11:56:28 +0800

    PM-32:feat:resource新增 接口调试

--- pm-controller/src/main/java/com/pm/controller/ResourceController.java
+++ pm-controller/src/main/java/com/pm/controller/ResourceController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +27,7 @@ public class ResourceController {
     private IResourceService resourceService;
 
     @PostMapping
-    public SingleResponse<String> addResource(ResourceAddCmd addCmd) {
+    public SingleResponse<String> addResource(@RequestBody @Validated ResourceAddCmd addCmd) {
         return SingleResponse.of(resourceService.addResource(addCmd));
     }
 
